054d9e680b7e35c03a3c7a40f9a0f8f1fc8d3533
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-18 21:45:42 +0200

    Use lyb_sidebar for bookings and accounts sidebars.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,7 @@
 class AccountsController < AuthorizedController
+  # Sidebars
+  sidebars = ['bookings/sidebar']
+  
   # Scopes
   has_scope :by_value_period, :using => [:from, :to], :default => proc { |c| c.session[:has_scope] }
   has_scope :by_text
